98c4e5c6c0b503fda8b491f9984ac5279384a3e7
Shraddhap26 <114189753+Shraddhap26@users.noreply.github.com>
2023-10-02 12:49:19 +0530

    Time: 678 ms (9.04%), Space: 10.6 MB (27.9%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,4 +1,7 @@
 //Lecture : 24
+// if num=10 ,  0. 1. 2 3 4. 5 6. 7 8. 9. 10.
+//Consider 0 and 1 as a non prime number
+//first we remove all the multiple of 2, then 3,5,7
 class Solution {
 public:
     int countPrimes(int n) {
